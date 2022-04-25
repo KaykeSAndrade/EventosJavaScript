9351c60b4150ff5c6658dc48451e64597d92b7c3
Kayke SAndrade <kaykewwe1212@hotmail.com>
2022-04-25 09:18:04 -0300

    first commit

---
+++ eventos.js
@@ -0,0 +1,24 @@
+function cliqueMe(){
+
+    console.log("Fui Clicado!");
+}
+
+function FocusMe(){
+
+    console.log("Fui Focado!");
+}
+
+function BlurMe(){
+
+    console.log("Fui desfocado!");
+}
+
+function changeMe(){
+
+    console.log("Fui Alterado!");
+}
+
+function overMe(){
+
+    console.log("Mouse Aqui BB!");
+}
